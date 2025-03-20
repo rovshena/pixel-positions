ea32ea83549655eea768b32ea177f9d9b02cacb4
Rovshen Annayev <rovshen.dev@gmail.com>
2025-03-20 23:19:14 +0500

    feat: blade and tailwind techniques for your laravel views

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,15 +1,15 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+<x-panel class="flex flex-col text-center">
     <div class="self-start text-sm">Laracasts</div>
-    <div class="py-8 font-bold">
-        <h3>Video Producer</h3>
-        <p>Full Time - From $60,000</p>
+    <div class="py-8">
+        <h3 class="text-xl font-bold group-hover:text-blue-800 transition-colors duration-300">Video Producer</h3>
+        <p class="text-sm mt-4">Full Time - From $60,000</p>
     </div>
     <div class="flex items-center justify-between mt-auto">
         <div>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
+            <x-tag size="small">Backend</x-tag>
+            <x-tag size="small">Frontend</x-tag>
+            <x-tag size="small">Manager</x-tag>
         </div>
-        <img src="https://placehold.co/42x42" class="rounded-xl" alt="">
+        <x-employer-logo :width="42"/>
     </div>
-</div>
+</x-panel>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,6 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel Positions</title>
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;500;600&display=swap">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
